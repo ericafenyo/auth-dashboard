61598a45b0869ccba77df59673e74d3b08107dd4
eric <ericafenyo@yahoo.com>
2021-02-13 19:33:54 +0100

    did minor code clean up

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -13,14 +13,14 @@
       :name="name"
       :disabled="disabled"
       :value="value"
-      @input="(args) => $emit('input', args)"
+      @input="args => $emit('input', args)"
     />
     <span
       v-if="hasHint"
       :class="[
         'input-help-message',
         { 'input-help-message-error': hasError },
-        { hidden: !hasError },
+        { hidden: !hasError }
       ]"
       >Field is required</span
     >
@@ -28,38 +28,38 @@
 </template>
 
 <script lang="ts">
-import { Vue, Prop, Component } from 'vue-property-decorator';
+import { Vue, Prop, Component } from "vue-property-decorator";
 
-import InputText from './InputText.vue';
+import InputText from "./InputText.vue";
 
 @Component({
   components: {
-    InputText,
-  },
+    InputText
+  }
 })
 export default class Input extends Vue {
-  @Prop({ type: String, default: '' })
+  @Prop({ type: String, default: "" })
   readonly type!: string;
 
-  @Prop({ type: String, default: '' })
+  @Prop({ type: String, default: "" })
   readonly label!: string;
 
-  @Prop({ type: String, default: '' })
+  @Prop({ type: String, default: "" })
   readonly placeholder!: string;
 
-  @Prop({ type: String, default: '' })
+  @Prop({ type: String, default: "" })
   readonly helperText!: string;
 
   @Prop({ type: Boolean, default: false })
   readonly hasError!: boolean;
 
-  @Prop({ type: String, default: '' })
+  @Prop({ type: String, default: "" })
   readonly size!: string;
 
   @Prop({ type: String, default: null })
   readonly value!: string;
 
-  @Prop({ type: String, default: '' })
+  @Prop({ type: String, default: "" })
   readonly name!: string;
 
   @Prop({ type: Boolean, default: false })
@@ -71,10 +71,10 @@ export default class Input extends Vue {
   @Prop({ type: Boolean, default: false })
   readonly disabled!: boolean;
 
-  @Prop({ type: String, default: '' })
+  @Prop({ type: String, default: "" })
   readonly actionText!: string;
 
-  @Prop({ type: String, default: '' })
+  @Prop({ type: String, default: "" })
   readonly actionRoute!: string;
 
   capitalize(value: string) {
@@ -83,18 +83,18 @@ export default class Input extends Vue {
 
   computeClasses(errors: any[]) {
     const { state } = this.$props;
-    return (state ? `input-${state}` : '') + (errors[0] ? ' input-error' : '');
+    return (state ? `input-${state}` : "") + (errors[0] ? " input-error" : "");
   }
 
   get computeComponent(): string {
     const { capitalize, type } = this;
-    return type ? `Input${capitalize(type)}` : 'InputText';
+    return type ? `Input${capitalize(type)}` : "InputText";
   }
 }
 </script>
 
 <style lang="scss" scoped>
-@import '@/scss/_mixins';
+@import "@/scss/_mixins";
 
 @mixin InputPlaceholderStyle {
   ::placeholder {
@@ -112,11 +112,11 @@ export default class Input extends Vue {
     color: #6b7280;
     font-weight: 500;
     text-transform: uppercase;
-    letter-spacing: -0.25px;
+    letter-spacing: -0.15px;
   }
 
   /deep/ input {
-    height: 40px;
+    height: 37px;
     font-size: 0.875rem;
     color: var(--color-primary-text);
     background: var(--color-surface-lightest);
@@ -124,7 +124,6 @@ export default class Input extends Vue {
     padding-left: 0.75rem;
     padding-right: 0.75rem;
     border-radius: 4px;
-    width: 100%;
     z-index: 0;
     transition: all 0.2s;
 
